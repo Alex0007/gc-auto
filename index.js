92bfd87b1b9de4535cf17b7f3beda727dee8f63c
Alex0007 <i@alex0007.ru>
2015-08-27 19:54:05 +0500

    now you can pass interval to constructor

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 var MemCleaner = (function() {
 
+    var instance
+
     if (global.gc) {
         console.log('Garbage collector is available on global scope')
     } else {
         console.log('--expose-gc flag wasn\'t provided')
     }
 
-    var instance = global.gc ? setInterval(function() {
-        try {
-            global.gc()
-        } catch (gcerr) {
-            console.log('Garbage collecting error', gcerr)
-        }
-    }, 30000) : undefined
+    var set = function(interval) {
+        return global.gc ? setInterval(function() {
+            try {
+                global.gc()
+            } catch (gcerr) {
+                console.log('Garbage collecting error', gcerr)
+            }
+        }, interval) : undefined
+    }
 
-    return function Construct_singletone() {
-        if (instance) {
-            return instance
+    var createInstance = function(interval) {
+        interval = interval || 30000
+
+        if (interval < 1000) {
+            console.log('gc cleaner interval set to 1 second')
+            interval = 1000
         }
+        if (instance) {
+            clearInterval(instance)
+        }
+
+        return set(interval)
+    }
+
+    instance = set(30000)
+
+    return function Construct_singletone(interval) {
+
+        console.log('Setting garbage collector periodic task with interval', interval, 'ms')
+
+        instance = createInstance(interval)
+        return instance
+
         if (this && this.constructor === Construct_singletone) {
             instance = this
         } else {
